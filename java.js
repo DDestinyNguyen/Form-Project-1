455bb551214289d66168b80465d6edb90a191167
NguyenHuuHai <93021270+DDestinyNguyen@users.noreply.github.com>
2022-06-21 11:13:11 +0700

    Add files via upload
    Sửa file javascript thêm ContactNumber

--- java.js
+++ java.js
@@ -7,15 +7,20 @@ var md = new Remarkable({
 	quotes: "“”‘’"
 });
 function submit() {
-	var name =
-		document.getElementById("fname").value +
-		" " +
-		document.getElementById("lname").value;
+	var name = document.getElementById("fname").value + " " +document.getElementById("lname").value;
+	
+	var contactNumber = document.getElementById("contact-number").value;
+	
 	var email = document.getElementById("email").value;
+	
 	var reason = $("input[name=reason]:checked", "#reason").val();
+	
 	var notparsedcomment = document.getElementById("comment").value;
+	
 	var comment = md.render(notparsedcomment);
-	var text = `Name: ${name}<br>Email: ${email}<br>Reason of contact: ${reason}<br>Comment: ${comment}`;
+	
+	var text = `Name: ${name}<br>ContactNumber: ${contactNumber}<br>Email: ${email}<br>Reason of contact: ${reason}<br>Comment: ${comment}`;
+	
 	document.getElementById("form").style.display = "none";
 	document.getElementById("result").style.display = "block";
 	document.getElementById("result").innerHTML = text;
@@ -28,3 +33,5 @@ $("textarea").keyup(function () {
 		.next()
 		.html(this.value.length + "/" + $(this).attr("maxlength"));
 });
+
+
